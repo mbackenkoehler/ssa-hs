b76fcc2035eac947b3479a9d159b3b84bf629407
Michael Backenköhler <m.backenkoehler@gmail.com>
2016-09-17 14:55:54 +0200

    move from optparse-generic to optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards    #-}
 ------------------------------------------------------------------------------
 {-|
   Module      : Main
@@ -20,35 +18,71 @@ import           Control.Monad ( void )
 import           Control.Monad.Trans.RWS.Strict ( execRWST )
 import           Data.Maybe
 import           GHC.Generics
-import           Options.Generic
+import           Options.Applicative
 ------------------------------------------------------------------------------
 import qualified SSA.Parser as SSA
 import qualified SSA.Model  as SSA
 import qualified SSA.SSA    as SSA
 ------------------------------------------------------------------------------
-data Args = Args
-  { model       :: String         <?> "Model specification file"
-  , tmax        :: Double         <?> "Time to run the simulation"
-  , csv         :: Maybe FilePath <?> "CSV output of simulation values"
-  , granularity :: Maybe Double   <?> "Logging intervals"
-  , verbose     :: Maybe Bool     <?> "Verbosity during simulation"
-  , warmup      :: Maybe Double   <?> "Warmup time before logging"
-  } deriving (Generic, Show)
+data Verbosity = Normal | Verbose deriving (Eq)
 
-instance ParseRecord Args
+data Args = Args
+  { model       :: String         -- Model specification file
+  , tmax        :: Double         -- Time to run the simulation
+  , csv         :: Maybe FilePath -- CSV output of simulation values
+  , granularity :: Maybe Double   -- Logging intervals
+  , verbose     :: Verbosity      -- Verbosity during simulation
+  , warmup      :: Maybe Double   -- Warmup time before logging
+  }
 ------------------------------------------------------------------------------
+args :: Parser Args
+args = Args
+  <$> strOption
+      ( long "model"
+     <> short 'm'
+     <> metavar "FILE"
+     <> help "Model specification file" )
+  <*> option auto
+      ( long "tmax"
+     <> short 't'
+     <> metavar "TMAX"
+     <> help "Time to run the simulation" )
+  <*> optional (strOption
+      ( long "output"
+     <> short 'o'
+     <> metavar "LOG_FILE"
+     <> help "csv output file" ))
+  <*> optional (option auto
+      ( long "granularity"
+     <> short 'g'
+     <> metavar "dt"
+     <> help "Logging intervals" ))
+  <*> flag Normal Verbose
+      ( long "verbose"
+     <> short 'v'
+     <> help "Enable verbose simulation" )
+  <*> optional (option auto
+      ( long "warmup"
+     <> short 'w'
+     <> help "Warmup time before logging starts" ))
+
+opts = info (helper <*> args)
+   ( fullDesc
+  <> progDesc "Simulate a chemical reaction network specified in FILE"
+  <> header "ssa-hs - Stochastic Simulation Algorithm" )
+
 settings :: Args -> SSA.Model -> (SSA.SimulationSettings,SSA.SimState)
 settings Args{..} s =
   ( SSA.SimulationSettings
     { SSA.system = s
-    , SSA.tmax = unHelpful tmax
-    , SSA.granularity = fromMaybe 10 (unHelpful granularity)
-    , SSA.verbose = fromMaybe True (unHelpful verbose)
-    , SSA.logFile = unHelpful csv
+    , SSA.tmax = tmax
+    , SSA.granularity = fromMaybe 10 granularity
+    , SSA.verbose = verbose == Verbose
+    , SSA.logFile = csv
     }
   , SSA.SimState
     { SSA.state = SSA.initial s
-    , SSA.time = - fromMaybe 0 (unHelpful warmup)
+    , SSA.time = - fromMaybe 0 warmup
     , SSA.tlast = 0
     , SSA.slast = SSA.initial s
     }
@@ -56,8 +90,8 @@ settings Args{..} s =
 
 main :: IO ()
 main = do
-  args <- getRecord "Stochastic Simulation Algorithm"
-  let modelFile = unHelpful (model args)
+  args <- execParser opts
+  let modelFile = model args
   modelStr <- readFile modelFile
   case SSA.parseModel modelFile modelStr of
     Left err -> print err
